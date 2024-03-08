14151a542cb0ffeebf6d7a1ed15107d62bf66589
HowardPlayzOA <66976582+HPOA909@users.noreply.github.com>
2024-03-07 20:55:21 -0800

    More shenanigans

---
+++ Nots and Codes/devider.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+void devider(float num1,float num2);
+
+int main(){
+    float num1;
+    float num2;
+    cout << "Enter your numerator: " << endl;
+    cin >> num1;
+    cout << "Ether your denominator: " << endl;
+    cin >> num2;
+
+    devider(num1,num2);
+
+    return 0;
+}
+
+void devider(float num1,float num2){
+    float result;
+    result = num1 / num2;
+    cout << "The result of those numbers that you have put in is " << result << endl;
+}
