4f91bbf6475fcfdce01a03a1caa2280db4f3ea53
HowardPlayzOA <66976582+HPOA909@users.noreply.github.com>
2024-04-02 21:06:01 -0700

    what the average joe are these

---
+++ Nots and Codes/examScores.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    const SIZE = 5;
+    double scores[SIZE];
+    const double AVERAGE = 5;
+    double average;
+    
+    for (int inex=0;index<SIZE;index++){
+        cout << "Enter your scores: " << index+1 << endl;
+        cin >> scores[index];
+    }
+    cout << "Here is the average score of 5: " << endl;
+    for (int index=0;index<SIZE;index++){
+        average = (scores[index]++) / AVERAGE;
+        cout << "Average score"
+    }
+
+    return 0;
+}
