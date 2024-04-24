e9be49ead54e3762eba115cec56a0c1321f7c832
HowardPlayzOA <66976582+HPOA909@users.noreply.github.com>
2024-04-23 19:34:45 -0700

    more shizzes

---
+++ gas prices.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <fstream>
+using namespace std;
+
+int main(){
+    int day, month, year;
+    double price;
+
+    ofstream gasFile;
+    gasFile.open("prices.txt");
+
+    for (int index=0;index<=5;index++){
+        
+    }
+
+    return 0;
+}
