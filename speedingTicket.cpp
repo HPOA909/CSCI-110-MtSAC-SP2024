52da2e0b31a18c037e4883b56efee36fed40ba04
HowardPlayzOA <66976582+HPOA909@users.noreply.github.com>
2024-05-01 10:24:56 -0700

    I'm done

---
+++ speedingTicket.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+/* OS: Linux 6.6.15-amd64
+   Compiler: GCC */
+
+void Ticket(int speed1, int speed2){
+
+}
+
+int main(){
+    int speed1, speed2;
+    string plate;
+    const int fee=150;
+
+    cout << "Enter vehicle plate number: " << endl;
+    cin >> plate;
+
+    cout << 
+
+    return 0;
+}
